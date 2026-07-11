3d652101e49bc9e084deaf0d417017e064497452
Eric Nguyen <lamnguyen.eric@gmail.com>
2026-07-11 17:43:43 -0600

    Fix button type for Save action in ProjectForm component

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -23,7 +23,9 @@ export default function ProjectForm({ onSave, onCancel }: ProjectFormProps) {
       <label htmlFor="isActive">Active?</label>
       <input type="checkbox" name="isActive" />
       <div className="input-group">
-        <button className="primary bordered medium">Save</button>
+        <button className="primary bordered medium" type="submit">
+          Save
+        </button>
         <span />
         <button type="button" className="bordered medium" onClick={onCancel}>
           cancel
